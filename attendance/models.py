a666b00d9ec8b4d9c94c547672e7d15c75d1d75f
Zach <zac.sergeant@gmail.com>
2014-10-14 16:30:21 -0700

    Added models for person and check-in

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class Person(AbstractUser):
+    teacher = models.BooleanField(default=False)
+
+
+class CheckIn(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Person, related_name='attendancecount')
